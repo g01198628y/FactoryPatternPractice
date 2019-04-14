8660f8a43ce52354917cb40d21478b83b7c72857
gary.lai99 <gary.lai99@TI03NB133>
2019-04-14 16:22:38 +0800

    Sell BlackTea.

--- FactoryPatternPracticeTwo/Program.cs
+++ FactoryPatternPracticeTwo/Program.cs
@@ -10,16 +10,43 @@ namespace FactoryPatternPracticeTwo
     {
         private static void Main(string[] args)
         {
-            OrderTea();
+            OrderBlackTea();
         }
 
-        public static GreenTea OrderTea()
+        public static GreenTea OrderGreenTeaTea()
         {
             GreenTea greenTea = new GreenTea();
             greenTea.AddMaterial(); //加料
             greenTea.Brew(); // 沖泡
             greenTea.PouredCup(); // 裝杯
             return greenTea;
+        }
+
+        public static BlackTea OrderBlackTea()
+        {
+            BlackTea blackTea = new BlackTea();
+            blackTea.AddMaterial(); //加料
+            blackTea.Brew(); // 沖泡
+            blackTea.PouredCup(); // 裝杯
+            return blackTea;
+        }
+    }
+
+    internal class BlackTea
+    {
+        public void AddMaterial()
+        {
+            Console.WriteLine("加料");
+        }
+
+        public void Brew()
+        {
+            Console.WriteLine("沖泡");
+        }
+
+        public void PouredCup()
+        {
+            Console.WriteLine("裝杯");
         }
     }
 
