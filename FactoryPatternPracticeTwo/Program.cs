3061f1e0ba1479b3c436647e030cf7069db20dd6
gary.lai99 <gary.lai99@TI03NB133>
2019-04-14 16:47:34 +0800

    Extract "AddMaterial" "Brew" "PouredCup" To Interface.

--- FactoryPatternPracticeTwo/Program.cs
+++ FactoryPatternPracticeTwo/Program.cs
@@ -16,55 +16,64 @@ namespace FactoryPatternPracticeTwo
         public static GreenTea OrderGreenTeaTea()
         {
             GreenTea greenTea = new GreenTea();
-            greenTea.AddMaterial(); //加料
-            greenTea.Brew(); // 沖泡
-            greenTea.PouredCup(); // 裝杯
+            greenTea.AddMaterial();
+            greenTea.Brew();
+            greenTea.PouredCup();
             return greenTea;
         }
 
         public static BlackTea OrderBlackTea()
         {
             BlackTea blackTea = new BlackTea();
-            blackTea.AddMaterial(); //加料
-            blackTea.Brew(); // 沖泡
-            blackTea.PouredCup(); // 裝杯
+            blackTea.AddMaterial();
+            blackTea.Brew();
+            blackTea.PouredCup();
             return blackTea;
         }
     }
 
-    internal class BlackTea
+    public class GreenTea : IMakeBeverage
     {
         public void AddMaterial()
         {
-            Console.WriteLine("加料");
+            Console.WriteLine("加入綠茶包");
         }
 
         public void Brew()
         {
-            Console.WriteLine("沖泡");
+            Console.WriteLine("加入水沖泡");
         }
 
         public void PouredCup()
         {
-            Console.WriteLine("裝杯");
+            Console.WriteLine("將綠茶裝杯");
         }
     }
 
-    internal class GreenTea
+    public class BlackTea : IMakeBeverage
     {
         public void AddMaterial()
         {
-            Console.WriteLine("加料");
+            Console.WriteLine("加入紅茶包");
         }
 
         public void Brew()
         {
-            Console.WriteLine("沖泡");
+            Console.WriteLine("加入水沖泡");
         }
 
         public void PouredCup()
         {
-            Console.WriteLine("裝杯");
+            Console.WriteLine("將紅茶裝杯");
         }
+    }
+
+    internal interface IMakeBeverage
+    {
+        void AddMaterial();
+
+        void Brew();
+
+        void PouredCup();
     }
 }
